cd38f74ceaca7dd5d487d10265aa1e9f34a594bb
Jatinsinh0102 <jatinsinhsolanki0102@gmail.com>
2026-09-10 19:26:58 +0530

    Move animation to 0.4s (200ms leave + 200ms enter)

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -53,7 +53,7 @@ export default function TaskCard({ task, onMove, onDelete, incoming }) {
     setLeaving(true)
     moveTimer.current = setTimeout(() => {
       onMove(task.id, nextStatus)
-    }, 150)
+    }, 200)
   }
 
   const handleDragStart = (e) => {

cc328006a8f162af99814de8c2540ca8791a9d54
Jatinsinh0102 <jatinsinhsolanki0102@gmail.com>
2026-09-10 18:57:30 +0530

    0.1s card move animation (50ms leave + 50ms enter), optimistic

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -53,7 +53,7 @@ export default function TaskCard({ task, onMove, onDelete, incoming }) {
     setLeaving(true)
     moveTimer.current = setTimeout(() => {
       onMove(task.id, nextStatus)
-    }, 0)
+    }, 50)
   }
 
   const handleDragStart = (e) => {
